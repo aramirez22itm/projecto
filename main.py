8c66be578566cee9f4ede9701ff91049ac550e02
aramirez22itm <alexanderramirez1131113@correo.itm.edu.co>
2025-07-30 21:20:39 -0500

    se definio el archivo para guardar nltk, los stopwords

---
+++ main.py
@@ -0,0 +1,8 @@
+# importar la libreria NLTK
+import nltk
+
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+
+# definir la ruta donde se almacenaran los datos descargados de nltk
+nltk.data.path.append('C:/Users/User/AppData/Roaming/nltk_data')
